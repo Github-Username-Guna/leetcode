037f72f7e072b8c705246f31c9be87df257a6690
Guna-Sekhar-Reddy <79553865+Github-Username-Guna@users.noreply.github.com>
2023-09-04 06:30:38 +0530

    Create arrange by gender.sql

---
+++ sql/ddl/arrange by gender.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE Gender
+( userid INT NOT NULL
+, gender nvarchar(max)
+
+) ;
+
+INSERT INTO Gender VALUES (4,'male') ;
+INSERT INTO Gender VALUES (7,'female') ;
+INSERT INTO Gender VALUES (2,'other') ;
+INSERT INTO Gender VALUES (5,'male' ) ;
+INSERT INTO Gender VALUES (3,'female' ) ;
+
+INSERT INTO Gender VALUES (8,'male' ) ;
+INSERT INTO Gender VALUES (6,'other' ) ;
+INSERT INTO Gender VALUES (1,'other' ) ;
+INSERT INTO Gender VALUES (9,'female') ;
