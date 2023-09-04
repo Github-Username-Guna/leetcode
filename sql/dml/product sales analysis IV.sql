61b6e612dec21af84d49d510d514809a342e543f
Guna-Sekhar-Reddy <79553865+Github-Username-Guna@users.noreply.github.com>
2023-09-04 07:51:43 +0530

    Update product sales analysis IV.sql

--- sql/dml/product sales analysis IV.sql
+++ sql/dml/product sales analysis IV.sql
@@ -1,1 +1,6 @@
 
+select s.productid,s.userid,sum(s.quantity*p.price) as Price,
+RANK() OVER (partition by s.userid order by sum(s.quantity*p.price)) as ranking
+from Sales s
+left join Product p  on s.productid=p.productid 
+group by s.productid,s.userid
