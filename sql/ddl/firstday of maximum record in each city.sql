be3f422f342d067d7d1d817e2be1e4f035a9d535
Guna-Sekhar-Reddy <79553865+Github-Username-Guna@users.noreply.github.com>
2023-09-04 07:25:28 +0530

    Create firstday of maximum record in each city.sql

---
+++ sql/ddl/firstday of maximum record in each city.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Weather
+( cityid INT NOT NULL
+, degree INT NOT NULL
+ ,day date
+) ;
+
+INSERT INTO Weather VALUES (1,'2022-01-07',-12) ;
+INSERT INTO Weather VALUES (1,'2022-03-07',5) ;
+INSERT INTO Weather VALUES (1,'2022-07-07',24) ;
+INSERT INTO Weather VALUES (2, '2022-08-07',37) ;
+INSERT INTO Weather VALUES (2, '2022-08-17',37) ;
+
+
+INSERT INTO Weather VALUES (3, '2022-02-07',-7) ;
+INSERT INTO Weather VALUES (3, '2022-12-07',-6) ;
