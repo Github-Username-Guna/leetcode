516bbf54dd65559e554d65535476b4320b1172bc
Guna-Sekhar-Reddy <79553865+Github-Username-Guna@users.noreply.github.com>
2023-09-05 12:21:11 +0530

    Create date wise distinct users.sql

---
+++ sql/ddl/date wise distinct users.sql
@@ -0,0 +1,10 @@
+create table user_activity(
+  date date,user_id int);
+
+insert into user_activity values('2022-02-20',1);
+insert into user_activity values('2022-02-20',2);
+insert into user_activity values('2022-02-22',1);
+insert into user_activity values('2022-02-22',3);
+insert into user_activity values('2022-02-24',1);
+insert into user_activity values('2022-02-24',2);
+insert into user_activity values('2022-02-24',3);
