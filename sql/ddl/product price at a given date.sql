3510d3e47e621698069f419783a26bd4aa5abd5f
Guna-Sekhar-Reddy <79553865+gunasekharreddy97@users.noreply.github.com>
2023-09-27 11:09:05 +0530

    Create product price at a given date.sql

---
+++ sql/ddl/product price at a given date.sql
@@ -0,0 +1,16 @@
+---Find the Price of all the products on a specified date , all the inital prices of the product is 10
+---ex - the price of products on 2019-08-16 is
+--productid  price
+--2          50
+--1          35
+--3          10
+
+
+Create table If Not Exists Products (product_id int, new_price int, change_date date);
+
+insert into Products (product_id, new_price, change_date) values ('1', '20', '2019-08-14');
+insert into Products (product_id, new_price, change_date) values ('2', '50', '2019-08-14');
+insert into Products (product_id, new_price, change_date) values ('1', '30', '2019-08-15');
+insert into Products (product_id, new_price, change_date) values ('1', '35', '2019-08-16');
+insert into Products (product_id, new_price, change_date) values ('2', '65', '2019-08-17');
+insert into Products (product_id, new_price, change_date) values ('3', '20', '2019-08-18');
