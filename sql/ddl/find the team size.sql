12c0f09e8d6acb074ed30b4712b06d80ec69ac23
Guna-Sekhar-Reddy <79553865+gunasekharreddy97@users.noreply.github.com>
2023-09-30 13:59:31 +0530

    Create find the team size.sql

---
+++ sql/ddl/find the team size.sql
@@ -0,0 +1,19 @@
+--- Find the teamsize for employee
+--EmployeeID  Teamsize
+--1           3
+--2           3
+--3           3
+--4           1
+--5           2
+--6           2
+
+
+Create table  Employee (employee_id int, team_id int);
+
+
+insert into Employee (employee_id, team_id) values ('1', '8');
+insert into Employee (employee_id, team_id) values ('2', '8');
+insert into Employee (employee_id, team_id) values ('3', '8');
+insert into Employee (employee_id, team_id) values ('4', '7');
+insert into Employee (employee_id, team_id) values ('5', '9');
+insert into Employee (employee_id, team_id) values ('6', '9');
